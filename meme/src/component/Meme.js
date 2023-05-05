1946042358f26fcc43421ec72d4cef82698b7a9a
Mykola Vorotynskyy <vorotynskyy@gmail.com>
2023-05-05 14:22:37 +0200

    commit

--- meme/src/component/Meme.js
+++ meme/src/component/Meme.js
@@ -12,9 +12,13 @@ export default function Meme() {
 const [allMemes, setAllMemes] = React.useState([])
     
 React.useEffect(() => {
-    fetch("http://api.imgflip.com/get_memes")
-    .then(res => res.json())
-    .then(data => setAllMemes(data.data.memes))
+    async function getMemes() {
+    const res = await fetch("http://api.imgflip.com/get_memes")
+    const data = await res.json()   
+    setAllMemes(data.data.memes) 
+    }
+  getMemes()
+   
 }, [])
 
    function getMemeImage() {
